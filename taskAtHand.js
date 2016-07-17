c2450ce9aa37cb6bc5007a4e5363d72ba0af7e6b
WapmasterRohan <rohangayen@gmail.com>
2016-07-18 03:14:04 +0530

    Initial commit to pushing project over to github

---
+++ taskAtHand.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function TaskAtHandApp() {
+    var version = "v1.0";
+
+    function setStatus(message) {
+        $("#app > footer").text(message);
+    }
+
+    this.start = function() {
+        $("#app > header").append(version);
+        setStatus("ready");
+    };
+}
+
+$(function() {
+    window.app = new TaskAtHandApp();
+    window.app.start();
+});
